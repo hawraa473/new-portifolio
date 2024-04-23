b92cddab14506e2aa4edc9f07387f35ccb046498
hawraa473 <hawraamuayad473@gmail.com>
2024-04-23 12:49:28 +0300

    commit

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.nav-bar');
+// let menuIcon = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('.nav-bar');
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+// menuIcon.onclick = () => {
+//     menuIcon.classList.toggle('bx-x');
+//     navbar.classList.toggle('active');
    
 }
