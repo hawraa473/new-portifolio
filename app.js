e22b914e444877a6892e6d2c6ceae97e25a604a1
hawraa473 <hawraamuayad473@gmail.com>
2024-04-23 08:06:13 +0300

    just a little responsive

---
+++ app.js
@@ -0,0 +1,8 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.nav-bar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+   
+}
